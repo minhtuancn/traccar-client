81d11cd1d5abb618be9a4161cced520934364515
Minh Tuấn <vietkeynet@gmail.com>
2026-09-17 09:00:01 +0700

    feat(android): expose watchdog control to Flutter

--- android/app/src/main/kotlin/org/traccar/client/MainActivity.kt
+++ android/app/src/main/kotlin/org/traccar/client/MainActivity.kt
@@ -1,5 +1,32 @@
 package org.traccar.client
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            WATCHDOG_CHANNEL,
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "arm" -> {
+                    TrackingWatchdogScheduler.schedule(applicationContext)
+                    result.success(null)
+                }
+                "cancel" -> {
+                    TrackingWatchdogScheduler.cancel(applicationContext)
+                    result.success(null)
+                }
+                else -> result.notImplemented()
+            }
+        }
+    }
+
+    companion object {
+        private const val WATCHDOG_CHANNEL = "traccar_client/watchdog"
+    }
+}

d995a6d2ef353d78eeb355d03618961d00097c22
Minh Tuấn <vietkeynet@gmail.com>
2026-09-17 11:40:18 +0700

    feat: reapply fork enhancements after sdk config writes

--- lib/geolocation_service.dart
+++ lib/geolocation_service.dart
@@ -1,5 +1,17 @@
 import 'package:traccar_client_sdk/traccar_client_sdk.dart';
 
+import 'enhanced_tracking_service.dart';
+
 class GeolocationService {
   static final tracker = TraccarClientSdk();
+
+  static Future<void> init(Config config) async {
+    await tracker.init(config);
+    await EnhancedTrackingService.apply();
+  }
+
+  static Future<void> setConfig(Config config) async {
+    await tracker.setConfig(config);
+    await EnhancedTrackingService.apply();
+  }
 }
